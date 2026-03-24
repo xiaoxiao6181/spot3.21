414fd159d9b1e06775d404ff7ec3b1afe6d3baab
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-03-24 22:21:55 +0800

    Delete assets directory

--- assets/useAuth-CtWFFltf.js
+++
@@ -1,1 +0,0 @@
-import{a3 as a,r as u,a4 as s}from"./index-C380uu9A.js";const h=a("auth",()=>{const t=u();return{auth:s({get:()=>t.value,set:e=>{t.value=e}}),_auth:t}},{persist:!0});export{h as u};
--- assets/useMoney-OuIFBvOe.js
+++
@@ -1,1 +0,0 @@
-import{a3 as t,r as s,a4 as n}from"./index-C380uu9A.js";const m=t("money",()=>{const e=s();return{money:n({get:()=>e.value,set:o=>{e.value=o}}),_money:e}},{persist:!0});export{m as u};
